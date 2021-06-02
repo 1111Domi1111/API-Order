80c748ae3a7e4a0677838bd3f76c5d309c751958
1111Domi1111 <70098260+1111Domi1111@users.noreply.github.com>
2021-06-02 07:03:17 +0300

    Update Const.cs

--- Const.cs
+++ Const.cs
@@ -1,16 +1,1 @@
-using System.Runtime.InteropServices;
 
-namespace WebApplication1.Const
-{
-    public class Constant
-    {
-        public static string adressCoctail = "https://the-cocktail-db.p.rapidapi.com";
-        public static string apiKeyCoctail = "05f5fb1962msh2360769d177a0d8p1ce5dcjsn2ed406057f08";
-        public static string adressDish = "https://tasty.p.rapidapi.com";
-        public static string apiKeyDish = "05f5fb1962msh2360769d177a0d8p1ce5dcjsn2ed406057f08";
-        public static string AccessKey = "AKIARAV6657RZQK4SPPQ";
-        public static string Secret = "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";
-        public static string TableName1 = "Order";
-        public static string TableName2 = "Discount";
-    }
-}
